2a296dd5a11a8549c1652d36462b52d9c6b6ed0f
Jean-Bernard Jansen <jean-bernard.jansen@dataiku.com>
2018-07-16 15:32:36 +0200

    all modules: Update the metadata

--- library/dss_connection_postgresql.py
+++ library/dss_connection_postgresql.py
@@ -3,7 +3,7 @@
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
-    'supported_by': 'dataiku-tools'
+    'supported_by': 'dataiku-ansible-modules'
 }
 
 DOCUMENTATION = '''
